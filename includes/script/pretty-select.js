cfa80b15abb4622b62752e40b75f487d6245c5e6
Bart Hendrix <info@barthendrix.com>
2019-10-16 11:44:53 +0200

    moved files and folders around, script is pretty much complete

--- includes/script/pretty-select.js
+++ includes/script/pretty-select.js
@@ -1,3 +1,5 @@
+// based on https://www.w3schools.com/howto/howto_custom_select.asp
+
 function prettySelect(select) {
   var comp = 'pretty-select';
 
@@ -5,6 +7,7 @@ function prettySelect(select) {
   var wrapper = document.createElement('div');
   wrapper.classList.add(comp);
   select.parentNode.insertBefore(wrapper, select);
+  var selectClassNames = select.getAttribute('class');
   select.classList.add(comp + '__select');
   wrapper.appendChild(select);
   wrapper.classList.add(comp + '--inactive')
@@ -16,27 +19,42 @@ function prettySelect(select) {
   wrapper.appendChild(selectedOption);
 
   var clonedSelect = document.createElement('div');
+  if(selectClassNames) {
+    // console.log(optionClassNames)
+    clonedSelect.setAttribute('class', selectClassNames);
+  }
   clonedSelect.classList.add(comp + '__clone');
-  for (var i = 1; i < select.length; i++) {
-    /* For each option in the original select element,
-    create a new DIV that will act as an option item: */
+  for (var i = 0; i < select.length; i++) {
+
+    // For each option in the original select element,
+    // create a new DIV that will act as an option item:
     var optionDiv = document.createElement('div');
     optionDiv.innerHTML = select.options[i].innerHTML;
+
+    // transplant existing classnames on option elements to the newly created div
+    var optionClassNames = select.options[i].getAttribute('class');
+    if(optionClassNames) {
+      // console.log(optionClassNames)
+      optionDiv.setAttribute('class', optionClassNames);
+    }
+
     if(select.options[i] == select.options[select.selectedIndex]) {
       optionDiv.classList.add(comp + '__option--selected');
     }
+
     optionDiv.addEventListener('click', function(e) {
-      /* When an item is clicked, update the original select box,
-      and the selected item: */
+      // When an item is clicked, update the original select box,
+      // and the selected item
       var selectEl = this.parentNode.parentNode.getElementsByTagName('select')[0];
       var activeDiv = this.parentNode.previousSibling;
+
       for (var j = 0; j < selectEl.length; j ++) {
         if (selectEl.options[j].innerHTML == this.innerHTML) {
           selectEl.selectedIndex = j;
           activeDiv.innerHTML = this.innerHTML;
           var selectedOption = this.parentNode.getElementsByClassName(comp + '__option--selected');
           for (var k = 0; k < selectedOption.length; k++) {
-            selectedOption[k].removeAttribute('class');
+            selectedOption[k].classList.remove(comp + '__option--selected');
           }
           this.classList.add(comp + '__option--selected');
           break;
@@ -49,14 +67,27 @@ function prettySelect(select) {
   wrapper.appendChild(clonedSelect);
 
   selectedOption.addEventListener('click', function(e) {
-    /* When the select box is clicked, close any other select boxes,
-    and open/close the current select box: */
-    e.stopPropagation();
-    closeAllSelect(this);
+     // When the select box is clicked, open/close the current select box
     this.parentNode.classList.toggle(comp + '--inactive');
   });
 
-  function closeAllSelect(element) {
-    
-  }
+  // listen for clickoutside events on the pretty select. Close it when user clicks outside
+  // avoid using e.stopPropagation(), see https://css-tricks.com/dangers-stopping-event-propagation/
+  // thanks to https://www.blustemy.io/detecting-a-click-outside-an-element-in-javascript/
+  document.addEventListener('click', function(e) {
+    var targetElement = e.target;  // clicked element
+
+    do {
+      if (targetElement == wrapper) {
+        // clicked inside, simply return
+        return;
+      }
+      // Go up the DOM
+      targetElement = targetElement.parentNode;
+    } while (targetElement);
+
+    // Clicked outside.
+    wrapper.classList.add(comp + '--inactive');
+});
+
 }
